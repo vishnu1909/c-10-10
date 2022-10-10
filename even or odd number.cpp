fa0f5ed9e2a3460d905ca153549fd6c8c4141234
vishnu1909 <113003675+vishnu1909@users.noreply.github.com>
2022-10-10 09:16:57 +0530

    Add files via upload

---
+++ even or odd number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"enter the number";
+	cin>>n;
+	if(n%2==0){
+		cout<<"even number";
+	}
+	else
+	{
+		cout<<"odd number";
+	}
+}
