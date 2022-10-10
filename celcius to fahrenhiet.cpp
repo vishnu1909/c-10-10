72d1e4819d6898cc1a29e335bda62c2e8c4c46db
vishnu1909 <113003675+vishnu1909@users.noreply.github.com>
2022-10-10 09:10:43 +0530

    Add files via upload

---
+++ celcius to fahrenhiet.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int c,t;
+	cout<<"enter the temperature in celcius";
+	cin>>c;
+	t=(c*(9/5)+32);
+	cout<<"the temperature in fahrenhiet"<<t;
+}
